5f8a87fb8269901b005d2a00f3d1e9c9bb2a1324
KonstantinVVictoria <konstantinvictoria@gmail.com>
2024-01-07 01:14:35 -0800

    added images and sections for splash page

--- pages/SplashPage/SplashPage.tsx
+++ pages/SplashPage/SplashPage.tsx
@@ -47,8 +47,84 @@ export default function SplashPage() {
               margin: "0.5rem 0rem",
             }}
           >
-            It’s not just <b>entertainment</b> <br />
-            It’s about the <b>experience</b>
+            <i>"</i> It’s not just <b>entertainment.</b> <br />
+            It’s about the <b>experience.</b>
+            <i>"</i>
+          </p>
+        </section>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "1rem 0rem",
+            padding: "1rem",
+            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.3)",
+            color: "var(--obsidian)",
+          }}
+        >
+          <img
+            src="/images/whatwedo.png"
+            style={{ width: "100%", objectFit: "cover", borderRadius: "15px" }}
+          />
+          <h2
+            style={{
+              fontWeight: "400",
+              fontSize: "2rem",
+              textAlign: "center",
+            }}
+          >
+            What We Do
+          </h2>
+          <p
+            style={{
+              fontWeight: "200",
+              fontSize: "1.7rem",
+              textAlign: "center",
+              margin: "0.5rem 0rem",
+            }}
+          >
+            We offer a full Wedding and Event entertainment service including
+            Lighting and Photo Booth options.
+          </p>
+        </section>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "1rem 0rem",
+            padding: "1rem",
+            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.3)",
+            color: "var(--obsidian)",
+          }}
+        >
+          <img
+            src="/images/dj_kev_luu_side.jpg"
+            style={{ width: "100%", objectFit: "cover", borderRadius: "15px" }}
+          />
+          <h2
+            style={{
+              fontWeight: "400",
+              fontSize: "2rem",
+              textAlign: "center",
+            }}
+          >
+            Who We Are
+          </h2>
+          <p
+            style={{
+              fontWeight: "200",
+              fontSize: "1.7rem",
+              textAlign: "center",
+              margin: "0.5rem 0rem",
+            }}
+          >
+            An entertainment company run by professional musicians and audio
+            engineers where customer satisfaction and attention to detail is our
+            highest priority.
           </p>
         </section>
       </main>
@@ -68,10 +144,10 @@ export default function SplashPage() {
         }}
       >
         <input
-          placeholder="Search"
+          placeholder="Ask an AI"
           style={{
             height: " 2.5rem",
-            border: "1px solid var(--concrete)",
+            border: "none",
             borderRadius: "7px",
             width: "80%",
             background: "transparent",
