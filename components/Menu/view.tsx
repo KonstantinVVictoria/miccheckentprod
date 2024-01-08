f2df59d9a2f3966a26c8831e4725712031c30501
KonstantinVVictoria <konstantinvictoria@gmail.com>
2024-01-07 20:14:07 -0800

    Added menu items

--- components/Menu/view.tsx
+++ components/Menu/view.tsx
@@ -1,8 +1,46 @@
 "use client";
-import { ExitIcon, MenuIcon } from "../Icons/view";
+import {
+  AboutUsIcon,
+  CalendarIcon,
+  ExitIcon,
+  HomeIcon,
+  JobIcon,
+  MenuIcon,
+} from "../Icons/view";
 import styles from "./styles.module.css";
 import { MenuProps } from "./types";
 import { useEffect, useState } from "react";
+
+type MenuButtonProps = {
+  children: React.ReactNode;
+  label: string;
+  special: boolean;
+};
+function MenuButton(props: MenuButtonProps) {
+  return (
+    <button
+      style={{
+        border: "none",
+        background: "transparent",
+        fontFamily: "inherit",
+      }}
+    >
+      {props.children}
+      <p
+        style={{
+          margin: 0,
+          textAlign: "center",
+          fontFamily: "inherit",
+          fontSize: "1rem",
+          color: props.special ? "var(--ruby)" : "",
+        }}
+      >
+        {props.label}
+      </p>
+    </button>
+  );
+}
+
 export default function Menu() {
   const [is_menu_open, menu_open] = useState(false);
   useEffect(() => {
@@ -66,7 +104,57 @@ export default function Menu() {
           pointerEvents: is_menu_open ? "auto" : "none",
           backdropFilter: is_menu_open ? "blur(60px)" : "blur(0px)",
         }}
-      ></div>
+      >
+        <menu
+          style={{
+            position: "absolute",
+            top: "0",
+            margin: "0",
+            width: "100%",
+            height: "calc(100vh - 4rem)",
+            padding: "1rem",
+            zIndex: "104",
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <MenuButton label="Home">
+            <HomeIcon
+              style={{ height: "4rem", width: "auto" }}
+              color={"var(--obsidian)"}
+            />
+          </MenuButton>
+          <MenuButton label="Book" special>
+            <CalendarIcon
+              style={{ height: "4rem", width: "auto" }}
+              color={"var(--ruby)"}
+            />
+          </MenuButton>
+          <MenuButton label="About Us">
+            <AboutUsIcon
+              style={{ height: "4rem", width: "auto" }}
+              color={"var(--obsidian)"}
+            />
+          </MenuButton>
+          <MenuButton label="Contact Us">
+            <HomeIcon
+              style={{ height: "4rem", width: "auto" }}
+              color={"var(--obsidian)"}
+            />
+          </MenuButton>
+          <MenuButton label="Jobs">
+            <JobIcon
+              style={{ height: "4rem", width: "auto" }}
+              color={"var(--obsidian)"}
+            />
+          </MenuButton>
+        </menu>
+      </div>
+
       <footer
         style={{
           zIndex: 103,
@@ -101,7 +189,7 @@ export default function Menu() {
               zIndex: 104,
               position: "relative",
             }}
-            color={"var(--marble)"}
+            color={"var(--obsidian)"}
           />
         </button>
       </footer>
